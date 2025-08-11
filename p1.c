6fa7a94158e87a44fd3c10da79b08b69f7c56266
Bode <bode@syn-2603-6010-8900-3658-0000-0000-0000-16a7.res6.spectrum.com>
2025-08-11 00:22:34 -0400

    added loop to reach send and mutex stuff that is in p2.c

--- p1.c
+++ p1.c
@@ -5,10 +5,16 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+
 int p1, join;
 
 unsigned char send_message[256];
 unsigned char receive_message[256];
+pthread_mutex_t lock;
+int send_flag = 0;
+int exit_flag = 0;
+
 
 
 void xor(char *msg,size_t msg_len, const char *key, size_t key_len){
@@ -24,28 +30,36 @@ void *send_msg(void *arg) {
     int send_sock = *(int *)arg;
     unsigned char key[256];
 
+    while (exit_flag != 1) {
+        pthread_mutex_lock(&lock);
+        if (send_flag == 1) {
+            printf("Input message: \n");
+            fgets((char *)msg,256,stdin);
+            printf("Input key: \n");
+            fgets(key,sizeof(key),stdin);
 
-    printf("Input message: \n");
-    fgets((char *)msg,256,stdin);
-    printf("Input key: \n");
-    fgets(key,sizeof(key),stdin);
+            msg[strcspn((char *)msg, "\n")] = '\0';
+            key[strcspn((char *)key, "\n")] = '\0';
 
-    msg[strcspn((char *)msg, "\n")] = '\0';
-    key[strcspn((char *)key, "\n")] = '\0';
+            size_t msg_len = strlen((char *)msg);
+            size_t key_len = strlen(key);
 
-    size_t msg_len = strlen((char *)msg);
-    size_t key_len = strlen(key);
+            unsigned char encrypted[256];
 
-    unsigned char encrypted[256];
+            memcpy(encrypted,msg,msg_len);
+            xor(encrypted,msg_len,key,key_len);
 
-    memcpy(encrypted,msg,msg_len);
-    xor(encrypted,msg_len,key,key_len);
+            printf("Me:%s \n",(char *)msg);
 
-    printf("Me:%s \n",(char *)msg);
+            size_t sent = send(send_sock,encrypted,msg_len,0);
+            send_flag = 0;
 
-    size_t sent = send(send_sock,encrypted,msg_len,0);
-    if (sent == -1) {
-        printf("[x] Message failed to send \n");
+            if (sent == -1) {
+                printf("[x] Message failed to send \n");
+            }
+        }
+        pthread_mutex_unlock(&lock);
+        usleep(100000);
     }
     return NULL;
 }
@@ -57,20 +71,23 @@ void *receive_msg(void *arg) {
     size_t encrypted_len = sizeof(encrypted_msg);
 
     size_t data_recieved;
-    while ((data_recieved = recv(receive_sock,encrypted_msg,encrypted_len,0)) > 0 ) {
-        printf("Input key: \n");
-        fgets(key,sizeof(key),stdin);
-        key[strcspn(key, "\n")] = '\0';
-        size_t key_len = strlen(key);
+    while (1) {
+        data_recieved = recv(receive_sock,encrypted_msg,encrypted_len,0);
+        if (data_recieved > 0) {
+            printf("Input key: \n");
+            fgets(key,sizeof(key),stdin);
+            key[strcspn(key, "\n")] = '\0';
+            size_t key_len = strlen(key);
 
-        xor((char *)encrypted_msg,data_recieved,key,key_len);
+            xor((char *)encrypted_msg,data_recieved,key,key_len);
 
-        unsigned char decrypted_msg[256];
-        for (int i = 0; i < data_recieved; i++) {
-            decrypted_msg[i] = encrypted_msg[i];
+            unsigned char decrypted_msg[256];
+            for (int i = 0; i < data_recieved; i++) {
+                decrypted_msg[i] = encrypted_msg[i];
+            }
+            decrypted_msg[data_recieved] = '\0';
+            printf("Anonymous: %s\n", (char *)decrypted_msg);
         }
-        decrypted_msg[data_recieved] = '\0';
-        printf("Anonymous: %s\n", (char *)decrypted_msg);
     }
     return NULL;
 }
@@ -99,6 +116,25 @@ int main() {
     pthread_t send, receive;
     pthread_create(&send,NULL,send_msg,(void *)&join);//args passed in last ,
     pthread_create(&receive,NULL,receive_msg,(void *)&join);//args passed in last ,
+
+    int stdin_flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, stdin_flags | O_NONBLOCK);
+
+    while (1) {
+        char input[20];
+        printf("Type ` to send a message or exit to EXIT \n");
+        if (fgets(input,sizeof(input),stdin)!= NULL) {
+            input[strcspn(input, "\n")] = '\0';
+            if (strcmp(input,"`") == 0) {
+                pthread_mutex_lock(&lock);
+                send_flag = 1;
+                pthread_mutex_unlock(&lock);
+            }else if (strcmp(input,"exit") == 0) {
+                // exit_flag = 1;
+                break;
+            }
+        }
+    }
 
     pthread_join(send,NULL);
     pthread_join(receive,NULL);
